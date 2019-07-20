49ce1790eda6587476b1a839eb79bd3b189fda49
matheussep17 <matheussep17@gmail.com>
2019-07-20 20:57:46 -0300

    POO aula 1 e desafio

--- Ruby/desafio4.rb
+++ Ruby/desafio4.rb
@@ -3,8 +3,8 @@ def expoente (n1,n2)
 end
 
 print 'Digite o numero base: '
-n1 = gets.chomp.to_i
+a = gets.chomp.to_i
 print 'Digite o expoente: '
-n2 = gets.chomp.to_i
+b = gets.chomp.to_i
 
-puts expoente(n1,n2)
+puts expoente(a,b)
